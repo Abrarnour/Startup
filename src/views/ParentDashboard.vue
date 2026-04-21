76ce8c4aa881d1fd33c2d1ed85fd14a9adbbadea
Abrarnour <abrarlacida@gmail.com>
2026-04-21 20:25:36 +0100

    parent etudiant dash trans

--- src/views/ParentDashboard.vue
+++ src/views/ParentDashboard.vue
@@ -616,7 +616,14 @@ onMounted(() => {
                   <Users :size="14" />{{ course.open_groups || 0 }} {{ t('groups_label') }}
                 </div>
               </div>
-
+              <button
+                @click="openEnrollModal(course)"
+                :disabled="children.length === 0"
+                class="w-full py-2.5 bg-gradient-to-r from-blue-500 to-purple-600 text-white rounded-lg font-semibold hover:from-blue-600 hover:to-purple-700 transition-all disabled:opacity-50 disabled:cursor-not-allowed"
+              >
+                {{ children.length > 0 ? t('enroll_child_free') : t('add_child_first') }}
+              </button>
+            </div>
           </div>
         </div>
       </div>
