5ca1da9203b8b95ca05b51e17c0090105900dcd3
Abrarnour <abrarlacida@gmail.com>
2026-05-12 22:16:56 +0100

    gddkjdjdddggkkkkkk

--- src/views/StudentDashboard.vue
+++ src/views/StudentDashboard.vue
@@ -122,8 +122,8 @@ onMounted(() => {
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="38"
-              height="38"
+              width="45"
+              height="45"
               viewBox="0 0 24 24"
               fill="none"
               stroke="white"
